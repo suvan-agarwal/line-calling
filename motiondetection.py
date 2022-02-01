41a19d4e9b64951e6d2d784c9b076a9bbaba775c
suvan <scrublinux@gmail.com>
2022-02-01 13:27:35 -0500

    motiondetection updated to be better

--- motiondetection.py
+++ motiondetection.py
@@ -5,10 +5,12 @@ import datetime
 import imutils
 import time
 import cv2
+import numpy as np
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
-ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+ap.add_argument("-m", "--max-area", type=int, default = 400, help="maximum area size")
+ap.add_argument("-a", "--min-area", type=int, default = 50, help="minimum area size")
 args = vars(ap.parse_args())
 # if the video argument is None, then we are reading from webcam
 if args.get("video", None) is None:
@@ -20,8 +22,8 @@ else:
 # initialize the first frame in the video stream
 firstFrame = None
 
-greenLower = (24, 70, 110)
-greenUpper = (29, 130, 255)
+greenLower = (20, 60, 0)
+greenUpper = (32, 255, 255)
 
 # loop over the frames of the video
 while True:
@@ -36,11 +38,6 @@ while True:
 		break
 	# resize the frame, convert it to grayscale, and blur it
 	frame = imutils.resize(frame, width=1000)
-	# blur = cv2.GaussianBlur(frame, (11, 11), 0)
-	# hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-	# mask = cv2.inRange(hsv, greenLower, greenUpper)
-	# maskblur = cv2.GaussianBlur(mask, (21, 21), 0)
-	# moving = cv2.bitwise_and(frame, frame, mask=mask)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	gray = cv2.GaussianBlur(gray, (21, 21), 0)
 	# if the first frame is None, initialize it
@@ -51,37 +48,47 @@ while True:
 # compute the absolute difference between the current frame and
 	# first frame
 	frameDelta = cv2.absdiff(firstFrame, gray)
-	thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+	thresh = cv2.threshold(frameDelta, 10, 255, cv2.THRESH_BINARY)[1]
 	# dilate the thresholded image to fill in holes, then find contours
 	# on thresholded image
-	thresh = cv2.dilate(thresh, None, iterations=10)
+	thresh = cv2.dilate(thresh, None, iterations=2)
 	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)
 	cnts = imutils.grab_contours(cnts)
 	# print(cnts)
 	# loop over the contours
 	for c in cnts:
+		# time.sleep(0.1)
 		# if the contour is too small, ignore it
 		if cv2.contourArea(c) < args["min_area"]:
+			continue
+		if cv2.contourArea(c) > args["max_area"]:
 			continue
 		# compute the bounding box for the contour, draw it on the frame,
 		# and update the text
 		(x, y, w, h) = cv2.boundingRect(c)
-		print(x,y)
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-		text = "Occupied"
+		box = frame[y:(y+h), x:(x+w)]
+		# print(x,(x+w),y,y+h	,box)
+		hsv = cv2.cvtColor(box, cv2.COLOR_BGR2HSV)
+		mask = cv2.inRange(hsv, greenLower, greenUpper)
+		mask = cv2.erode(mask, None, iterations=2)
+		mask = cv2.dilate(mask, None, iterations=2)
+		hasGreen = np.sum(mask)
+		if hasGreen > 1000:
+			cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+			cnts = imutils.grab_contours(cnts)
+			c = max(cnts, key=cv2.contourArea)
+			M = cv2.moments(c)
+			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+			print(center[0]+x, center[1]+y)
+			cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-	# # draw the text and timestamp on the frame
-	# cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-	# 	cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-	# cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-	# 	(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-	# show the frame and record if the user presses a key
 	cv2.imshow("Security Feed", frame)
 	cv2.moveWindow("Security Feed", 40,30)
 	cv2.imshow("Thresh", thresh)
 	cv2.moveWindow("Thresh", 1000,30)
-	cv2.imshow("Frame Delta", frameDelta)
+	cv2.imshow("Frame Delta", mask)
 	cv2.moveWindow("Frame Delta", 40,700)
 	key = cv2.waitKey(1) & 0xFF
 	# if the `q` key is pressed, break from the lop
